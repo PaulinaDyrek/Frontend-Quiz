b3ece0874376bb97c3558641ad5e1834045b1ac4
Paulina Dyrek <paoladlugosz@gmail.com>
2024-05-21 11:30:58 +0200

    new question in data.js

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,26 +1,52 @@
+import { useState } from "react";
+import { data } from "../../assets/data";
+import "../../components/Quiz/Quiz.css"
+
 export function Quiz() {
+  let [index, setIndex] = useState(0);
+  let [question, setQuestion] = useState(data[index]);
+  let [lock, setLock] = useState(false);
+
+  const checkAns = (e, ans) => {
+    if (lock === false ) {
+      if(question.ans===ans) {
+        e.target.classList.add("correct");
+        setLock(true);
+      }
+      else {
+        e.target.classList.add("wrong");
+        setLock(true);
+      }
+    }
+    if(question.ans===ans) {
+      e.target.classList.add("correct");
+    }
+    else {
+      e.target.classList.add("wrong");
+    }
+  }
   return (
-    <div className="w-[640px] m-auto mt-[150px] flex flex-col bg-brown text-grey gap-5 rounded-[10px] py-10 px-[50px]">
+    <div className="w-[540px] h-full m-auto mt-[50px] flex flex-col bg-brown text-grey gap-5 rounded-[10px] py-10 px-[50px]">
       <h1 className="text-[27px] font-bold m-auto ">Frontend Developer Quiz</h1>
       <hr className="h-0.5 bg-grey" />
-      <h2 className="text-[20px] font-medium">
-        Which of the following is not an HTML tag?
+      <h2 className="text-[18px] font-medium">
+        {index+1}. {question.question}
       </h2>
       <ul>
-        <li className="flex items-center h-[60px] pl-[15px] border-[1px] border-white divide-solid rounded-[10px] text-5 cursor cursor-pointer m-2">
-          doctype
+        <li onClick={(e)=>{checkAns(e,1)}} className="flex items-center h-[45px] pl-[15px] border-[1px] border-white divide-solid rounded-[10px] text-5 cursor cursor-pointer m-2">
+          {question.option1}
         </li>
-        <li className="flex items-center h-[60px] pl-[15px] border-[1px] border-white divide-solid rounded-[10px] text-5 cursor cursor-pointer  m-2">
-          table
+        <li onClick={(e)=>{checkAns(e,2)}} className="flex items-center h-[45px] pl-[15px] border-[1px] border-white divide-solid rounded-[10px] text-5 cursor cursor-pointer  m-2">
+          {question.option2}
         </li>
-        <li className="flex items-center h-[60px] pl-[15px] border-[1px] border-white divide-solid rounded-[10px] text-5 cursor cursor-pointer  m-2">
-          p
+        <li onClick={(e)=>{checkAns(e,3)}} className="flex items-center h-[45px] pl-[15px] border-[1px] border-white divide-solid rounded-[10px] text-5 cursor cursor-pointer  m-2">
+         {question.option3}
         </li>
-        <li className="flex items-center h-[60px] pl-[15px] border-[1px] border-white divide-solid rounded-[10px] text-5 cursor cursor-pointer  m-2">
-          style
+        <li onClick={(e)=>{checkAns(e,4)}} className="flex items-center h-[45px] pl-[15px] border-[1px] border-white divide-solid rounded-[10px] text-5 cursor cursor-pointer  m-2">
+          {question.option4}
         </li>
       </ul>
-      <button className="m-auto w-[200px] h-[50px] bg-lavenda  text-grey text-[20px] font-medium rounded-[8px] cursor-pointer">
+      <button className="m-auto w-[200px] h-[45px] bg-lavenda  text-grey text-[20px] font-medium rounded-[8px] cursor-pointer">
         Next
       </button>
       <div className="text-[14px] m-auto">1 of 5 questions</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
-        brown: "rgb(101, 69, 74)",
+        brown: "rgb(81, 54, 64)",
         grey: "rgb(225, 221, 221)",
         lavenda: "rgb(106, 88, 106)",
       },
